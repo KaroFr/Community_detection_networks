12fb4792df8e09d190d159975dd4b69678be2103
KaroFr <frohnapfelkarolin@gmail.com>
2024-06-28 17:05:28 +0200

    small changes

--- main_ev_SC.py
+++ main_ev_SC.py
@@ -8,14 +8,14 @@ from ErrorMeasures import LeiRinaldoMetric_1_fromLabels
 
 rho = 0.6
 n_nodes = 2000
-n_clusters = 3
+n_clusters = 5
 T = 100
 
 n_for = 10
 
 forgetting_factors = np.arange(0.0, 1.02, step=0.02)
 alphas = [0.01, 0.05, 0.1]
-epsilons = [0.1, 0.9]
+epsilons = [0.01, 0.025, 0.05, 0.075, 0.1]
 
 LeiRinaldoMetric_SC = 0
 LeiRinaldoMetric_evSC_arr = np.zeros(len(forgetting_factors))
