6d81a6dc266ca863b5aa5436e9e7b00f610b69a9
KaroFr <66319759+KaroFr@users.noreply.github.com>
2024-06-29 11:23:07 +0200

    grid search for optimal forgetting factor

--- main_ev_SC.py
+++ main_ev_SC.py
@@ -14,8 +14,9 @@ T = 100
 n_for = 10
 
 forgetting_factors = np.arange(0.0, 1.02, step=0.02)
-alphas = [0.01, 0.05, 0.1]
-epsilons = [0.01, 0.025, 0.05, 0.075, 0.1]
+alphas = [0.1]
+epsilons = np.arange(0.0, 1.01, step=0.01)
+# epsilons = [0.01, 0.025, 0.05, 0.075, 0.1]
 
 LeiRinaldoMetric_SC = 0
 LeiRinaldoMetric_evSC_arr = np.zeros(len(forgetting_factors))
@@ -26,8 +27,12 @@ for alpha in alphas:
 
         # load the results csv (if already existing) to save the variables
         try:
-            results_df = pd.read_csv('results/results_ev_SC.csv', sep=';', index_col=False)
+            results_df_1 = pd.read_csv('results/results_ev_SC.csv', sep=';', index_col=False)
         except FileNotFoundError:
+            ID = 1
+        try:
+            results_df_2 = pd.read_csv('results/results_ev_SC_optimal.csv', sep=';', index_col=False)
+        except:
             ID = 1
 
         for _ in tqdm(np.arange(n_for)):
@@ -60,7 +65,7 @@ for alpha in alphas:
         LeiRinaldoMetric_SC = LeiRinaldoMetric_SC / n_for
         LeiRinaldoMetric_evSC_arr = LeiRinaldoMetric_evSC_arr / n_for
 
-        # data frame of this run
+        # data frame of the whole run
         SBM_setting = SBM_object.get_values()
         SBM_setting['Metric_SC'] = LeiRinaldoMetric_SC
         results_ev_df = pd.DataFrame({'forgetting_factor ': forgetting_factors,
@@ -68,8 +73,20 @@ for alpha in alphas:
         results_ev_df = pd.concat([SBM_setting, results_ev_df], axis=1)
 
         try:
-            results_df = pd.concat([results_df, results_ev_df], ignore_index=True)
+            results_df_1 = pd.concat([results_df_1, results_ev_df], ignore_index=True)
         except NameError:
-            results_df = pd.concat([results_ev_df], ignore_index=True)
+            results_df_1 = pd.concat([results_ev_df], ignore_index=True)
 
-        results_df.to_csv('results/results_ev_SC.csv', sep=';', index=False)
+        results_df_1.to_csv('results/results_ev_SC.csv', sep=';', index=False)
+
+        # data frame of the optimal values only
+        opt_index = np.argmin(LeiRinaldoMetric_evSC_arr)
+        SBM_setting['opt_forgetting_factor'] = forgetting_factors[opt_index]
+        SBM_setting['opt_Metric_evSC'] = LeiRinaldoMetric_evSC_arr[opt_index]
+
+        try:
+            results_df_2 = pd.concat([results_df_2, SBM_setting], ignore_index=True)
+        except NameError:
+            results_df_2 = pd.concat([SBM_setting], ignore_index=True)
+
+        results_df_2.to_csv('results/results_ev_SC_optimal.csv', sep=';', index=False)
