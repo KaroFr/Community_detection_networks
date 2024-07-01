e815042dcb02219dd4e7266b2aa18faa1016edf9
KaroFr <66319759+KaroFr@users.noreply.github.com>
2024-07-01 20:56:30 +0200

    hyperparameter tuning in online setting

--- main_ev_SC.py
+++ main_ev_SC.py
@@ -3,19 +3,20 @@ import pandas as pd
 from tqdm import tqdm
 
 from SBM_Offline import SBM_Offline
+from SBM_Online import SBM_Online
 from SpectralClustering import SpectralClustering
 from ErrorMeasures import LeiRinaldoMetric_1_fromLabels
 
 rho = 0.6
 n_nodes = 2000
 n_clusters = 5
-T = 100
+T = 10
 
 n_for = 10
 
 forgetting_factors = np.arange(0.0, 1.02, step=0.02)
 alphas = [0.1]
-epsilons = np.arange(0.0, 1.01, step=0.01)
+epsilons = np.arange(0.0, 1.01, step=0.02)
 # epsilons = [0.01, 0.025, 0.05, 0.075, 0.1]
 
 LeiRinaldoMetric_SC = 0
@@ -37,32 +38,29 @@ for alpha in alphas:
 
         for _ in tqdm(np.arange(n_for)):
 
-            print('Simulate SBM')
-            SBM_object = SBM_Offline(n_clusters=n_clusters, n_nodes=n_nodes, rho=rho, alpha=alpha, n_time_steps=T, epsilon=epsilon)
-            SBMs = SBM_object.simulate()
-            labels_true = SBMs['labels'][T - 1]
-
-            # static Spectral Clustering
-            SC_object = SpectralClustering(adjacency=SBMs['adj_matrix'][T - 1], n_clusters=n_clusters, P_estimate='adjacency')
-            labels_SC = SC_object.performSC()
-            LeiRinaldoMetric_SC += LeiRinaldoMetric_1_fromLabels(labels_SC, labels_true)
-
             for index, forgetting_factor in enumerate(forgetting_factors):
+                print('Simulate SBM')
+                SBM_object = SBM_Online(n_clusters=n_clusters, n_nodes=n_nodes, rho=rho, alpha=alpha, epsilon=epsilon)
 
                 # calculate estimate for evolutionary SC
-                adj_matrix_estimate = SBMs['adj_matrix'][0]
+                labels_true, adj_matrix_estimate = SBM_object.simulate_next()
                 for t in np.arange(T):
-                    adj_matrix = SBMs['adj_matrix'][t]
+                    labels_true, adj_matrix = SBM_object.simulate_next()
                     adj_matrix_estimate = forgetting_factor * adj_matrix + (1 - forgetting_factor) * adj_matrix_estimate
 
-                # evolutionary Spectral Clustering
+                # evolutionary Spectral Clustering for last time step
                 SC_object_ev = SpectralClustering(adjacency=adj_matrix_estimate, n_clusters=n_clusters, P_estimate='adjacency')
                 labels_SC_ev = SC_object_ev.performSC()
-
-                # get misclustering error
                 LeiRinaldoMetric_evSC_arr[index] += LeiRinaldoMetric_1_fromLabels(labels_SC_ev, labels_true)
 
-        LeiRinaldoMetric_SC = LeiRinaldoMetric_SC / n_for
+                # static Spectral Clustering for last time step
+                SC_object = SpectralClustering(adjacency=adj_matrix, n_clusters=n_clusters, P_estimate='adjacency')
+                labels_SC = SC_object.performSC()
+                LeiRinaldoMetric_SC += LeiRinaldoMetric_1_fromLabels(labels_SC, labels_true)
+                print('index = ', index)
+
+        LeiRinaldoMetric_SC = LeiRinaldoMetric_SC / (n_for*len(forgetting_factors))
+        print(LeiRinaldoMetric_SC)
         LeiRinaldoMetric_evSC_arr = LeiRinaldoMetric_evSC_arr / n_for
 
         # data frame of the whole run
